16c0053e0059176099d1967dee48110e772a5a92
Itu-tree <toutounode@gmail.com>
2021-07-22 14:54:03 +0900

    add observersample

---
+++ Assets/UniRxSample/ObserverSample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace UniRxSample
+{
+    class ObserverSample :MonoBehaviour
+    {
+        private void Start()
+        {
+            //Obserberを手動生成
+            IObserver<string> observer = new DisplayLogObserver();
+
+            observer.OnNext("Hello");
+            observer.OnNext("World");
+            observer.OnCompleted();
+        }
+    }
+
+    public sealed class DisplayLogObserver : IObserver<string>
+    {
+        public void OnCompleted()
+        {
+            Debug.Log("ログの発行が完了しました。");
+        }
+
+        public void OnError(Exception error)
+        {
+            Debug.Log($"例外が発生しました:{error}");
+        }
+
+        public void OnNext(string value)
+        {
+            Debug.Log($"メッセージが発行されました：{value}");
+        }
+    }
+}
